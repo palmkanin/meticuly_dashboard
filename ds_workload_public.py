dd944bff5972855c1ba59a318cefc95aec4dd10e
palmkanin <80637728+palmkanin@users.noreply.github.com>
2022-11-10 22:13:12 +0700

    Update ds_workload_public.py
    add bday nday

--- ds_workload_public.py
+++ ds_workload_public.py
@@ -267,7 +267,9 @@ for i in range(len(ds_list)):
             st.subheader(f'{ds_list[i]} Workload Summary ')
             st.caption(f'from {start_date_f} - {end_date_f}')
             nday = pd.date_range(start_date, end_date)
-            st.caption(f'selected {len(nday)} days')
+            num_nday = abs(len(nday) - len(bday))
+            num_bday = len(bday)
+            st.caption(f'selected {len(nday)} days ( {num_nday} normal days | {num_bday} work days)')
             
         with block2:
             st.subheader('status: '+ mood)
